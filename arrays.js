fcf4164f977efd91758a07b5240be8f2d1c17ad2
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-09-15 12:20:43 -0500

    finished arrays.js

--- arrays.js
+++ arrays.js
@@ -4,12 +4,14 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
-
+const colorCopy = faveColors.slice()
+console.log(colorCopy);
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-
+let newcolorCopy = colorCopy.push('Yellow')
+console.log(newcolorCopy);
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -19,7 +21,8 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+let middleNums = numbers.splice(1,3)
+console.log(middleNums);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +37,14 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+let answers = []
+
+for (let i = 0; i <= bigOrSmallArray.length; i = i + 1) {
+  if (bigOrSmallArray > 100) {
+    console.log(answers.push['big'])
+  } else if (bigOrSmallArray <= 100) {
+    console.log(answers.push['small'])
+  } else {
+    console.log('invalid number')
+  }
+}
